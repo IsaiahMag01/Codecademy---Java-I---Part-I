7832cb774e8dffd199ff42f240eb63d3b2a9badd
Isaiah Magana <Isaiah2Mag@gmail.com>
2024-02-25 00:42:31 -0500

    Write public class and program documentation

---
+++ Scratch.java
@@ -0,0 +1,12 @@
+/**
+ * Write your first java program from scratch after completing the first section of the Java I course
+ * @author(IsaiahM)
+ * Scratch.java
+ */
+
+ public class Scratch
+ {
+   /**
+   main method to run program
+   */
+ }
